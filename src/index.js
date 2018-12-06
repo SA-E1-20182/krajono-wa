f4623f919fe3b5d72e5373feefe33db1cb5b0466
José Nieto <jdnv.123@gmail.com>
2018-12-05 21:07:53 -0500

    Declare Redux store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import { persistor, store } from './store';
+
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+
 import Home from './scenes/Home';
 import Auth from './scenes/Auth';
 import Signup from './scenes/Signup';
@@ -13,33 +20,36 @@ import Project from './scenes/Project';
 import NotFound from './scenes/NotFound';
 import Page from './scenes/Page';
 import CreateProject from './scenes/CreateProject';
-
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import AddVersion from './scenes/AddVersion';
 import Profile from './scenes/Profile';
 import EditProject from './scenes/EditProject';
 
-ReactDOM.render(
-    <BrowserRouter>
-        <div id="app">
-            <Navbar />
-            <main>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/auth" component={Auth} />
-                    <Route exact path="/author/:id" component={Profile} />
-                    <Route exact path="/project/create" component={CreateProject} />
-                    <Route exact path="/project/:id" component={Project} />
-                    <Route exact path="/project/:id/edit" component={EditProject} />
-                    <Route exact path="/signup" component={Signup} />
-                    <Route exact path="/version/add" component={AddVersion} />
-                    <Route exact path="/version/:id" component={Project} />
-                    <Route exact path="/project/:id/page/:num" component={Page} />
-                    <Route exact path="/404" component={NotFound} />
-                    <Redirect to="/404" />
-                </Switch>
-            </main>
-            <Footer />
-        </div>
-    </BrowserRouter>
-, document.getElementById('root'));
+ReactDOM.render((
+    <Provider store={store}>
+      <PersistGate loading={(() => <div>Loading</div>)()} persistor={persistor}>
+        <BrowserRouter>
+            <div id="app">
+                <Navbar />
+                <main>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/auth" component={Auth} />
+                        <Route exact path="/author/:id" component={Profile} />
+                        <Route exact path="/project/create" component={CreateProject} />
+                        <Route exact path="/project/:id" component={Project} />
+                        <Route exact path="/project/:id/edit" component={EditProject} />
+                        <Route exact path="/signup" component={Signup} />
+                        <Route exact path="/version/add" component={AddVersion} />
+                        <Route exact path="/version/:id" component={Project} />
+                        <Route exact path="/project/:id/page/:num" component={Page} />
+                        <Route exact path="/404" component={NotFound} />
+                        <Redirect to="/404" />
+                    </Switch>
+                </main>
+                <Footer />
+            </div>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+), document.getElementById('root'));
+
